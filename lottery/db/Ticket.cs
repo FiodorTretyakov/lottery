158ef6cf1b488c59610102a73b624b9aeda8275b
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-05-28 18:16:42 +0100

    Added attributes.

--- lottery/db/Ticket.cs
+++ lottery/db/Ticket.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace lottery.db
 {
     public class Ticket
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Data { get; set; }
     }
 }
