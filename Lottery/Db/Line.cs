75c7b533dc7eabc86656755252c0678fe31e084f
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-06-01 16:32:59 +0100

    Added validation for numbers.

--- Lottery/Db/Line.cs
+++ Lottery/Db/Line.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace Lottery.Db
 {
@@ -10,15 +11,41 @@ namespace Lottery.Db
         [NotMapped]
         public const int Size = 3;
 
+        [NotMapped]
+        public const int[] Allowed = [0, 1, 2];
+
+        private int ticketId;
+
         [Key]
         public int Id { get; set; }
 
         [ForeignKey("ticketId")]
         public Ticket ParentTicket { get; set; }
 
-        private int ticketId;
-
         public ICollection<int> Numbers { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                if (Numbers.Sum() == 2)
+                {
+                    return 10;
+                }
+
+                if (Numbers.All(n => n == Numbers.First()))
+                {
+                    return 5;
+                }
+
+                if (Numbers[1] != Numbers.First() && Numbers[2] != Numbers.First())
+                {
+                    return 1;
+                }
+
+                return 0;
+            }
+        }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime Inserted { get; set; }
@@ -28,6 +55,11 @@ namespace Lottery.Db
             if (numbers.Length != Size)
             {
                 throw new ArgumentOutOfRangeException($"Line length should be {Size}, but {numbers.Length}");
+            }
+
+            if (numbers.Any(n => !Allowed.Any(a => a == n)))
+            {
+                  throw new ArgumentOutOfRangeException($"There are only allowed values {string.Join(","Allowed)}, but {string.Join(",", numbers)}");              
             }
 
             Numbers = numbers;
