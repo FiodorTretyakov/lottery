a801f5965ba62e5bdf1660bea8f8cbfc99e50f5d
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-06-02 15:51:39 +0100

    Fixed.

--- Lottery/Db/TicketContext.cs
+++ Lottery/Db/TicketContext.cs
@@ -4,6 +4,13 @@ namespace Lottery.Db
 {
     public class TicketContext : DbContext
     {
+        public TicketContext()
+        {
+        }
+
+        public TicketContext(DbContextOptions<TicketContext> options) : base(options)
+        {
+        }
         public DbSet<Ticket> Tickets { get; set; }
         public DbSet<Line> Lines { get; set; }
     }
