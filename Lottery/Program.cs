018f854ab46306eb3d8fe5a6424ab9b3c77ca57d
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-06-04 22:34:55 +0100

    Fixed code coverage.

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Lottery
 {
+    [ExcludeFromCodeCoverage]
     public static class Program
     {
         public static async Task Main(string[] args) =>
